890e83aa7062e014ec542e0e6632982f3c30ad5d
Vagelis Kosmiadis <97461782+kosmiadis@users.noreply.github.com>
2026-06-10 19:51:42 +0300

    added try catch to usage example

--- main.cpp
+++ main.cpp
@@ -7,16 +7,33 @@ using namespace std;
 
 int main () {
     NaiveBayesModel model;
-    model.load_and_fit("dataset.csv");
 
-    PredictionData email1 = { "URGENT INVESTMENT ALERT! You have been selected to claim a free bonus in our crypto trading platform. Turn $100 into millions overnight with our guaranteed automated system. Click this link immediately to receive your reward cash prize before your access expires!" };
-    PredictionData email2 = { "Hi everyone, could you please review the latest updates on the project repository before tomorrow morning? We need to schedule a quick sync meeting with the development team to discuss the upcoming deployment and resolve any open bugs." };
+    try {
+        model.load_and_fit("dataset.csv");
+    } catch (const Error &err) {
+        cout << err;
+        return 0;
+    }
 
-    Prediction prediction1 = model.predict(email1);
-    Prediction prediction2 = model.predict(email2);
+    PredictionData email1 = { "URGENT INVESTMENT ALERT! You have been selected to claim a free bonus in our crypto trading platform" };
+    PredictionData email2 = { "free and hello there" };
 
-    std::cout << prediction1;
-    std::cout << prediction2;
+    try {
+
+        Prediction prediction1 = model.predict(email1);
+        std::cout << prediction1;
+    } catch (const Error &err) {
+        cout << err;
+        return 0;
+    }
+
+    try {
+        Prediction prediction2 = model.predict(email2);    
+        std::cout << prediction2;
+    } catch (const Error &err) {
+        cout << err;
+        return 0;
+    }
 
     return 0;
 }
